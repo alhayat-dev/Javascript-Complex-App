ef2ffe84982c6bcd0a7f6711d80676eacdd8518e
alhayat-dev <alhayat.dev@gmail.com>
2021-07-18 06:19:40 +0500

    Create .env file and placed secret credentials there.

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const mongodb = require('mongodb')
 
-let connectionString = 'mongodb+srv://mudasser:mudasser@123@cluster0.s61ft.mongodb.net/ComplexApp?retryWrites=true&w=majority'
-
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   module.exports = client.db()
   const app = require('./app')
-  app.listen(3000)
+  app.listen(process.env.PORT)
 })
