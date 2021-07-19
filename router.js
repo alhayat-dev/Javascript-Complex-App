7b6c079257e955a490dbac64fd7da2c582f35987
alhayat-dev <alhayat.dev@gmail.com>
2021-07-20 04:13:08 +0500

    Refactored code for user session, header and footer templates. Conditionally display the header for the logged in and logged out user.

--- router.js
+++ router.js
@@ -3,10 +3,15 @@ const router  = express.Router();
 
 // Requiring the controller
 const userController = require('./controllers/userController')
+const postController = require('./controllers/postController')
 
+// user related routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
+
+// post related routes
+router.get('/create-post', userController.mustBeLoggedIn ,postController.viewCreateScreen)
 
 module.exports = router
