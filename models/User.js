127192da02a3f31481fe7891d431e656f3bc6f76
alhayat-dev <alhayat.dev@gmail.com>
2021-07-20 00:18:19 +0500

    Displayed gravatar on user login and logout request.

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const userCollection = require('../db').db().collection('users')
-let validator = require('validator')
+const validator = require('validator')
+const md5 = require('md5')
 
 let User = function(data){
     this.data = data
@@ -52,6 +53,8 @@ User.prototype.login = function(){
         this.cleanUp()
         userCollection.findOne({username: this.data.username}).then((attemptedUser) => {
             if(attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)){
+                this.data = attemptedUser
+                this.getAvatar()
                 resolve('Congrats')
             }else{
                 reject('Invalid username / password.')
@@ -75,11 +78,16 @@ User.prototype.register = function(){
             let salt = bcrypt.genSaltSync(10)
             this.data.password = bcrypt.hashSync(this.data.password, salt)
             await userCollection.insertOne(this.data)
+            this.getAvatar()
             resolve()
         }else{
             reject(this.errors)
         }
     })
+}
+
+User.prototype.getAvatar = function(){
+    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 module.exports = User
