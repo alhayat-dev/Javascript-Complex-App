abfab3fb62895b3cf7bf561e7afc4432410c915c
alhayat-dev <alhayat.dev@gmail.com>
2021-07-20 17:03:46 +0500

    Added userId in session object.

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ exports.logout = function(req, res){
 exports.login = function(req, res){
     let user = new User(req.body)
     user.login().then(function(result){
-      req.session.user = { username: user.data.username, avatar: user.avatar}
+      req.session.user = { username: user.data.username, avatar: user.avatar, _id: user.data._id }
       req.session.save(function(){
         res.redirect('/')
       })
@@ -25,7 +25,7 @@ exports.login = function(req, res){
 exports.register = function(req, res) {
   let user = new User(req.body)
   user.register().then(() => {
-    req.session.user = {username: user.data.username, avatar: user.avatar}
+    req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
     req.session.save(function() {
       res.redirect('/')
     })
