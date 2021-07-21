cb5e25dfc571df88a06c8878dbb094cc8cd8455e
alhayat-dev <alhayat.dev@gmail.com>
2021-07-21 16:30:30 +0500

    Displayed all posts related to the current logged iin user on the profile page.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const session = require('express-session');
 const User = require('../models/User');
+const Post = require('../models/Post');
 
 exports.logout = function(req, res){
   req.session.destroy(function(){
@@ -68,8 +69,15 @@ exports.ifUserExists = function(req, res, next){
 }
 
 exports.profilePostsScreen = function(req, res){
-  res.render('profile',{
-    profileUsername: req.profileUser.username,
-    profileAvatar: req.profileUser.avatar
+  // ask our post model for posts by a certain author id
+
+  Post.findByAuthorId(req.profileUser._id).then(function(posts){
+    res.render('profile',{
+      posts: posts,
+      profileUsername: req.profileUser.username,
+      profileAvatar: req.profileUser.avatar
+    })  
+  }).catch(function(){
+    res.render("404")
   })
 }
