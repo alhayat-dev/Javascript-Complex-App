b37ed114f3f0c9606266925bbdad88ac38314233
alhayat-dev <alhayat.dev@gmail.com>
2021-07-21 05:31:34 +0500

    Display the user avatar and username on the user profile screen.

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,3 +58,18 @@ exports.mustBeLoggedIn = function(req, res, next){
   }
 }
 
+exports.ifUserExists = function(req, res, next){
+  User.findByUsername(req.params.username).then(function(userDocument){
+    req.profileUser = userDocument
+    next()
+  }).catch(function(){
+    res.render("404")
+  })
+}
+
+exports.profilePostsScreen = function(req, res){
+  res.render('profile',{
+    profileUsername: req.profileUser.username,
+    profileAvatar: req.profileUser.avatar
+  })
+}
