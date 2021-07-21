281c28a7e6f3b20420e97f3f4f434a23cf5ff3a1
alhayat-dev <alhayat.dev@gmail.com>
2021-07-21 16:50:26 +0500

    Hide edit and delete action buttons for a guest user.

--- models/Post.js
+++ models/Post.js
@@ -45,7 +45,7 @@ Post.prototype.create = function(){
   })
 }
 
-Post.reusablePostQuery = function(uniqueOperations){
+Post.reusablePostQuery = function(uniqueOperations, visitorId){
     return new Promise(async function(resolve, reject) {
         let aggOperations = uniqueOperations.concat([
             { $lookup: { from: "users", localField: "author", foreignField: "_id", as: "authorDocument" } },
@@ -53,6 +53,7 @@ Post.reusablePostQuery = function(uniqueOperations){
                 title: 1,
                 body: 1,
                 createdDate: 1,
+                authorId: "$author",
                 author: { $arrayElemAt: ["$authorDocument", 0] }
             }}
 
@@ -62,6 +63,8 @@ Post.reusablePostQuery = function(uniqueOperations){
 
         // clean up author property in each post object
         posts = posts.map(function(post){
+            post.isVisitorOwner = post.authorId.equals(visitorId)
+
             post.author = {
                 username: post.author.username,
                 avatar: new User(post.author, true).avatar
@@ -72,7 +75,7 @@ Post.reusablePostQuery = function(uniqueOperations){
     })
 }
 
-Post.findSingleById = function(id){
+Post.findSingleById = function(id, visitorId){
     return new Promise(async function(resolve, reject) {
         if(typeof(id) != "string" || !ObjectID.isValid(id)){
             reject()
@@ -81,7 +84,7 @@ Post.findSingleById = function(id){
 
         let posts = await Post.reusablePostQuery([
             {$match: { _id: new ObjectID(id) } }
-        ])
+        ], visitorId)
 
         if(posts.length){
             // console.log(posts[0])
